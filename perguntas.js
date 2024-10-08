c5dedac95b263841c294a3f33290d731ac8cbaa1
anasapaulasantos <ana.bispo.menezes@escola.pr.gov.br>
2024-10-08 12:23:17 -0300

    08/10

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programaçao',
+    'O que e JavaScript?',
+    'O JavaScript e uma linguagem de programaçao'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da frança?',
+    'A capital da frança e Paris',
+)
+
+criaCartao(
+    'Programaçao',
+    'O que e uma funçao?',
+    'Uma funçao e um bloco de codigos que executa uma funçao.'
+)
+
+criaCartao(
+    'Matematica',
+    'Qual e o resultado de 6 x 8?',
+    'O resultado e 48'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Italia?',
+    'A capital e Roma'
+)
+
+criaCartao(
+    'Ciencias',
+    'O sol e oque?',
+    'Uma estrela.'
+)
